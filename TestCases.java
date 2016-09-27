bde1229ac3d2cf58273e3dd7d50afed092ea9387
jdbravol <juanbravo@wireless-10-146-229-57.public.utexas.edu>
2016-09-27 17:51:18 -0500

    added empty test case

--- TestCases.java
+++ TestCases.java
@@ -1,12 +1,18 @@
 package assignment3;
 
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class TestCases {
 	
 	@Test
-	public static void Case1(){
-		
+	public static void EmptyCase(){
+		ArrayList<String> nullArray = new ArrayList<String>();
+		assertEquals(true, nullArray == Main.getWordLadderDFS("aloof", "smart"));
+		assertEquals(true, nullArray == Main.getWordLadderBFS("aloof", "smart"));
 	}
 
 }
