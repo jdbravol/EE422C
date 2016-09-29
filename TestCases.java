979b5a951b348fc67c4308ecf812a8777ca7f488
Vidhu Appalaraju <vidhuappalaraju@gmail.com>
2016-09-29 13:01:29 -0500

    added test cases

--- TestCases.java
+++ TestCases.java
@@ -15,4 +15,16 @@ public class TestCases {
 		assertEquals(true, nullArray == Main.getWordLadderBFS("aloof", "smart"));
 	}
 
+	@Test
+	public static void GeneralCase(){
+		//ArrayList<String> nullArray = new ArrayList<String>();
+		int count = 9;
+		assertEquals(true, 0 < Main.getWordLadderDFS("smart", "money").size()); //this tells us if the DFS reached the end word
+		assertEquals(true, count >= Main.getWordLadderBFS("smart", "money").size() - 2);//we know the ladder should be at most 9 because of the pdf
+	}
+	@Test
+	public static void SameWordCase(){
+		assertEquals(true, 1 == Main.getWordLadderDFS("smart", "smart").size()); //a 0 rung word ladder 
+		assertEquals(true, 1 == Main.getWordLadderBFS("smart", "smart").size());//
+	}
 }
