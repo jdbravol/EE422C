ff82b26a4f66218bc3cd256c1ea14caa986a2073
Vidhu Appalaraju <vidhuappalaraju@gmail.com>
2016-09-30 15:05:46 -0500

    forgot this

--- TestCases.java
+++ TestCases.java
@@ -9,22 +9,31 @@ import org.junit.Test;
 public class TestCases {
 	
 	@Test
-	public static void EmptyCase(){
+	public void EmptyCase(){
 		ArrayList<String> nullArray = new ArrayList<String>();
 		assertEquals(true, nullArray == Main.getWordLadderDFS("aloof", "smart"));
 		assertEquals(true, nullArray == Main.getWordLadderBFS("aloof", "smart"));
 	}
 
 	@Test
-	public static void GeneralCase(){
+	public void GeneralCase(){
 		//ArrayList<String> nullArray = new ArrayList<String>();
 		int count = 9;
 		assertEquals(true, 0 < Main.getWordLadderDFS("smart", "money").size()); //this tells us if the DFS reached the end word
 		assertEquals(true, count >= Main.getWordLadderBFS("smart", "money").size() - 2);//we know the ladder should be at most 9 because of the pdf
 	}
 	@Test
-	public static void SameWordCase(){
+	public void SameWordCase(){
 		assertEquals(true, 1 == Main.getWordLadderDFS("smart", "smart").size()); //a 0 rung word ladder 
 		assertEquals(true, 1 == Main.getWordLadderBFS("smart", "smart").size());//
 	}
+	@Test
+	public void GoingToAloofWords(){
+		ArrayList<String> nullArray = new ArrayList<String>();
+		assertEquals(true, nullArray == Main.getWordLadderDFS("smart", "aloof"));
+		assertEquals(true, nullArray == Main.getWordLadderBFS("smart", "aloof"));
+
+	}
+	
+	
 }
